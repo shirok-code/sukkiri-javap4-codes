0e5624efa75dad39ed41fa275676a7c7cf3679b2
Gakuto OE <ohe@miyabilink.jp>
2025-10-31 14:49:29 +0900

    コード3-6・3-7修正

--- chap03/code03-06/Main.java
+++ chap03/code03-06/Main.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Main {
-  public static void main(String args[]) {
+  public static void main(String[] args) {
     List<Hero> heroes = new ArrayList<>();
 
     /* heroesの例 */
